b902ef68c7120afad896470d8088560d0f1f63ad
connorguerin <connorguerin239@gmail.com>
2018-11-25 16:18:47 -0500

    Update Training Algorithm.py

--- Training Algorithm.py
+++ Training Algorithm.py
@@ -1,66 +1,63 @@
 import numpy as np
 from DataGenerator import DataGenerator
-from sklearn.linear_model import LinearRegression
 
 # tolerance for convergence of soln
-error = 0.00000001
+error = 0.000001
 # specified mu for phi(x)
 mu = 0.01
 #observation and feature numbers
-n = 2
-p = 5
+n = 5
+p = 10
 #generate data points
 data = DataGenerator(n, p)
 X, y, b = data.generatePoints()
 
 def main():
+    global mu
     # construct constraint matrix A
     A = constraintMatrix()
     # construct initial point
-    x0 = np.absolute(initial_point())
-    #resize to account for standard form variables
+    x0 = initial_point()
+    print(np.diagflat(x0))
+    #resize to account for slack variables
     x0.resize(2*p, 1, refcheck=False)
     for i in range(0, p):
         x0[i + p] = x0[i] * -1
     #calculate first search direction
     cfs = np.array(closedFormSolution(x0, A))
-    x1 = x0 + cfs
-
+    x1 = x0 + np.matmul(np.diagflat(x0),cfs)
     counter = 0
     #loop until specified tolerance is met
     while (abs((Fx(x1) - Fx(x0))) > error):
-        if counter > 100000:
-            print(counter)
-            print(Fx(x1))
-            break
-        x0 = x1
-        x1 = x1 + closedFormSolution(x1, A)
-        counter += 1
+            if (Fx(x1) > Fx(x0)):
+                mu /= 2
+            x0 = x1
+            x1 = x1 + np.matmul(np.diagflat(x1),closedFormSolution(x1, A))
+            counter += 1
 
-    print(x1)
-    print("\n")
+    print("MSE of true term and OLS solution")
+    print(round(np.square(np.subtract(initial_point(), b)).mean(), 6))
+    print("MSE of true term and algorithm solution")
+    print(round(np.square(np.subtract(x1[:p], b)).mean(), 6))
+    print(x1[:p])
     print(b)
+    print(initial_point())
+    print("iterations:" + str(counter))
 
-
-#function for calculating dt
 def closedFormSolution (x, A):
+    A=np.round(np.array(A), 3)
     x = np.array(x)
     diagX = np.diagflat(x)
-    A = np.array(A)
-    e = np.ones(x.size)
+    e = np.int8(np.ones(x.size))
     b = beta(mu)
-
-    penaltyTerm = np.array([term*mu for term in np.matmul(np.linalg.inv(diagX), e)])
-    gradientPhi = np.subtract(gradientFx(x).T, penaltyTerm)
-    pterm1 = np.linalg.inv(np.matmul(np.matmul(A,np.matmul(diagX,diagX)),A.T))
-    pterm2 = np.matmul(np.matmul(A,np.matmul(diagX,diagX)), gradientPhi.T)
-    p = np.matmul(pterm1, pterm2)
+    barrier = np.fromiter((term*mu for term in np.matmul(np.linalg.inv(diagX), e)), dtype=float)
+    gradientPhi = np.subtract(gradientFx(x).T, barrier)
+    p = np.matmul(np.linalg.inv(np.matmul(np.matmul(A,np.matmul(diagX,diagX)),A.T)), np.matmul(np.matmul(A,np.matmul(diagX,diagX)), gradientPhi.T))
     denom = np.linalg.norm(np.matmul(diagX, (gradientPhi.T - np.matmul(A.T, p))))
     if (denom != 0):
         num = np.matmul(np.matmul(diagX,diagX), (gradientPhi.T - np.matmul(A.T, p)))
         num = [(aTerm * b * (-1))/denom for aTerm in num]
         return num
-
     else:
         return 0
 
@@ -87,8 +84,8 @@ def gradientFx (xt):
     gamma = 0.2
     lam = 0.5
 
-    fPrime = np.matmul(X.T, (np.matmul(X, xt[:p]) - y))
-    fPrime = np.array([x / n for x in fPrime])
+    fPrime = np.subtract(np.matmul(np.matmul(X.T,X), xt[:p]), np.matmul(X.T,y))
+    fPrime = np.array([x * 2 for x in fPrime])
     fPrime = np.add(fPrime, 2* delta * (xt[:p] - b))
     mcp = np.zeros(p)
     for xi in range(0, p):
@@ -96,6 +93,7 @@ def gradientFx (xt):
             mcp[xi] = (lam - abs(xt[xi])/gamma)*np.sign(xt[xi])
 
     fPrime = np.add(fPrime.T, mcp)
+    #resize for slack variables
     fPrime.resize(p*2, 1, refcheck=False)
 
     return fPrime
@@ -109,22 +107,20 @@ def constraintMatrix ():
     constraint_one = np.zeros([p * 2])
     constraint_one[0] = 1
     constraint_one[p] = 1
-
-    A = np.array(constraint_one)
+    A = np.empty((p, 2 * p))
+    A[0] = np.array(constraint_one)
     B = np.array(constraint_one)
-    for i in range(0,p - 1):
-        for j in range(p*2 - 1, -1, -1):
+    for i in range(0, p - 1):
+        for j in range(p * 2 - 1, -1, -1):
             if B[j] <> 0:
                 B[j + 1] = B[j]
                 B[j] = 0
-        A = np.vstack((A, B))
-    return A
+        A[i + 1] = B
+    return np.int8(A)
 
 def initial_point():
-    reg = LinearRegression()
-    reg.fit(X, y)
-
-    return reg.coef_
+    s, residuals, rank, sing = np.linalg.lstsq(X,y, rcond=None)
+    return np.array(s)
 
 if __name__ == '__main__':
     main()
