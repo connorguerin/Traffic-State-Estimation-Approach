f1d9cbef0a0528653d4d5b74ada3692931939339
connorguerin <connorguerin239@gmail.com>
2018-12-30 13:38:23 -0500

    Update Training Algorithm.py

--- Training Algorithm.py
+++ Training Algorithm.py
@@ -2,27 +2,26 @@ import numpy as np
 from DataGenerator import DataGenerator
 
 # tolerance for convergence of soln
-error = 0.01
+error = 0.1
 # specified mu for phi(x)
 mu = 0.1
+
+L = 5
+b = mu / (L + 2 * mu)
 #observation and feature numbers
-n = 5
-p = 5
+n = 500
+p = 1000
 #generate data points
 data = DataGenerator(n, p)
-X, y, b = data.generatePoints()
+X, y, betaTrue = data.generatePoints()
 
 def main():
-    global mu
+    global b
     # construct constraint matrix A
     A = constraintMatrix()
     # construct initial point
     x0 = np.array(initial_point())
     #resize to account for slack variables
-    x0.resize(2*p, 1, refcheck=False)
-    for i in range(0, p):
-        x0[i] = abs(x0[i])
-        x0[i + p] = x0[i] * -1
     #calculate first search direction
     cfs = np.array(closedFormSolution(x0, A))
     x1 = np.add(x0,np.matmul(np.diagflat(x0),cfs))
@@ -30,21 +29,24 @@ def main():
     #loop until specified tolerance is met
     while (abs(Fx(x1)-Fx(x0)) > error):
             if (Fx(x1) > Fx(x0)):
-                mu /= 2
+                b /= 2
             x0 = x1
-            x1 = np.add(x1,np.matmul(np.diagflat(x1),cfs))
+            d = closedFormSolution(x0, A)
+            x1 = np.add(x1, np.matmul(np.diagflat(x1), d))
+            # if (np.linalg.norm(d) <= b ):
+            #     b /= 2
             counter+=1
-            print(abs(Fx(x1)-Fx(x0)))
             #print(abs(Fx(x1)-Fx(x0)))
+            print(str(abs(Fx(x1) - Fx(x0))) + " " + str(counter))
             if counter >= 1000:
                 break
     print("MSE of true term and OLS solution")
-    print(round(np.square(np.subtract(initial_point(), b)).mean(), 6))
+    print(round(np.square(np.subtract(initial_point()[:p], betaTrue)).mean(), 6))
     print("MSE of true term and algorithm solution")
-    print(round(np.square(np.subtract(x1[:p], b)).mean(), 6))
-    # print(x1[:p])
-    #print(initial_point())
-    print("iterations:" + str(counter))
+    print(round(np.square(np.subtract(x1[:p], betaTrue)).mean(), 6))
+    print(x1[:p])
+    print(betaTrue)
+    # print("iterations:" + str(counter))
     #print(Fx(x0))
     #print(Fx(x1))
 
@@ -54,14 +56,19 @@ def closedFormSolution (x, A):
     x = np.array(x)
     diagX = np.diagflat(x)
     e = np.int8(np.ones(x.size))
-    b = beta(mu)
     barrier = np.fromiter((term*mu for term in np.matmul(np.linalg.inv(diagX), e)), dtype=float)
     gradientPhi = np.subtract(gradientFx(x).T, barrier)
-    p = np.matmul(np.linalg.inv(np.matmul(np.matmul(A,np.matmul(diagX,diagX)),A.T)), np.matmul(np.matmul(A,np.matmul(diagX,diagX)), gradientPhi.T))
-    denom = np.linalg.norm(np.matmul(diagX, (gradientPhi.T - np.matmul(A.T, p))))
-    if (denom != 0):
-        num = np.matmul(np.matmul(diagX,diagX), (gradientPhi.T - np.matmul(A.T, p)))
-        num = [(aTerm * b * (-1))/denom for aTerm in num]
+    for i in range(0, x.size):
+        diagX[i][i] = diagX[i][i] ** 2
+    term = np.matmul(A, diagX)
+    p = np.linalg.inv(np.matmul(term, A.T))
+    l = np.matmul(term, gradientPhi.T)
+    p = np.matmul(p, l)
+    denom = np.matmul(np.diagflat(x), np.subtract(gradientPhi.T, np.matmul(A.T, p)))
+    if (np.linalg.norm(denom) != 0):
+        num = np.matmul(diagX, denom)
+        div = np.linalg.norm(denom)
+        num = [(aTerm * b * (-1)) / div for aTerm in num]
         return num
     else:
         return 0
@@ -69,25 +76,13 @@ def closedFormSolution (x, A):
 
 #method for defining function f(x)
 def Fx(xt):
-    # delta = 0.01
-    # gamma = 0.2
-    lam = 5000
+    lam = 500
     f = float(1)/(2*n)*(np.linalg.norm(np.subtract(np.matmul(X, xt[:p]),y))**2) + np.linalg.norm(xt[:p], ord=1)*lam
-    # f += delta * (np.linalg.norm((xt[:p] - b))**2)
-    # mcp = 0
-    # for xi in range(0, p):
-    #     if (abs(xt[xi]) <= gamma*lam):
-    #         mcp += lam*xt[xi]-(xt[xi]**2)/(2*gamma)
-    #     else:
-    #         mcp += (1/2)*gamma*(lam**2)
-    #     f += mcp
     return f
 
 #method for defining the gradient of the objective function
 def gradientFx (xt):
-    # delta = 0.01
-    # gamma = 0.2
-    lam = 5000
+    lam = 500
 
     fPrime = np.subtract(np.matmul(np.matmul(X.T,X), xt[:p]), np.matmul(X.T,y))
     fPrime = np.array([x * 2  for x in fPrime])
@@ -97,23 +92,9 @@ def gradientFx (xt):
             norm[i] = np.sign(xt[i])*lam
 
     fPrime = np.add(fPrime, norm)
-
-    # fPrime = np.add(fPrime, 2* delta * (xt[:p] - b))
-    # mcp = np.zeros(p)
-    # for xi in range(0, p):
-    #     if (abs(xt[xi]) <= gamma*lam):
-    #         mcp[xi] = (lam - abs(xt[xi])/gamma)*np.sign(xt[xi])
-    #
-    # fPrime = np.add(fPrime.T, mcp)
-    #resize for slack variables
     fPrime.resize(p*2, 1, refcheck=False)
 
     return fPrime
-
-#method calculates beta
-def beta (epsilon):
-    L = 50000
-    return epsilon/(L + 2 * epsilon)
 
 def constraintMatrix ():
     constraint_one = np.zeros([p * 2])
@@ -131,8 +112,12 @@ def constraintMatrix ():
     return np.int8(A)
 
 def initial_point():
-    s, residuals, rank, sing = np.linalg.lstsq(X,y, rcond=None)
-    return np.array(s)
+    point =  np.full([p,1], 5)
+    point.resize(2 * p, 1, refcheck=False)
+    for i in range(0, p):
+        # x0[i] = abs(x0[i])
+        point[i + p] = point[i] * -1
+    return point
 
 if __name__ == '__main__':
     main()
